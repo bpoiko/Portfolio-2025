930177e21bdd8ff61755547fa1005a6ba02041fe
Braylon <epicb35@gmail.com>
2025-05-30 23:07:16 -0700

    inital portfolio commit

---
+++ parker.js
@@ -0,0 +1,31 @@
+console.log("Hello world");
+
+let text = "Welcome to my portfolio!";
+let i = 0;
+
+let text2 = "I'm a Sophomore Computer Science student at Sacramento State. Thank you for taking your time to learn a bit more about me";
+let i2 = 0;
+
+function typeWriter() {
+    if (i < text.length) {
+        document.getElementById("typed").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeWriter, 75);
+    } else {
+        
+        setTimeout(secondWriter, 500);
+    }
+}
+
+function secondWriter() {
+    if (i2 < text2.length) {
+        document.getElementById("babble").innerHTML += text2.charAt(i2);
+        i2++;
+        setTimeout(secondWriter, 50);
+    }
+}
+
+
+window.onload = () => {
+    typeWriter();
+};
